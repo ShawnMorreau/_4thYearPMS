7572dac292c4b6a97d0c0307011d0f13231e51f2
Craig Isesele <cmoisesele@live.com>
2018-03-06 13:19:45 -0500

    added test class

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -1,9 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by CraigBook on 2018-03-06.
  */
 public class mainTest {
+    main main;
+    @Before
+    public void setUp() throws Exception {
+        main  = new main();
+    }
+    @Test
+    public void testReturn0() {
+        assertFalse(main.return0());
+    }
+
 
 
 }
