1e29b6ff11073c78fcbe45b957ebaf9e322dbc89
Craig Isesele <cmoisesele@live.com>
2018-03-06 13:08:33 -0500

    created test class

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,11 @@
 public class main {
     //it's here man
+
+    public boolean return0 () {
+        return false;
+    }
+
+    public static void main (String [] args) {
+
+    }
 }
---
+++ src/test/java/mainTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by CraigBook on 2018-03-06.
+ */
+public class mainTest {
+
+
+}
