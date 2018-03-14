694dfbb4ba29915687308299a249379f742988cb
Craig Isesele <cmoisesele@live.com>
2018-03-13 20:58:35 -0400

    modified index page and removed react support files

--- src/main/java/sysc4806/MainController.java
+++ src/main/java/sysc4806/MainController.java
@@ -1,39 +1,22 @@
 package sysc4806;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Created by CraigBook on 2018-03-13.
+ */
 @Controller
-@RequestMapping(path="/test")
+@RequestMapping(path="/")
 public class MainController {
 
-    @Autowired
-    private StudentRepo studentRepo;
-
-    @GetMapping(path="/add")
-    public @ResponseBody String addNewStudent (@RequestParam String name) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-        Student s = new Student();
-        s.setName(name);
-        studentRepo.save(s);
-        return "Saved: " + name;
-    }
-
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<Student> getAllStudents() {
+    @GetMapping(path="/")
+    public @ResponseBody
+    String returnHello() {
         // This returns a JSON or XML with the users
-        return studentRepo.findAll();
+        return "Welcome to Project Management System";
     }
 
-    @GetMapping(path="/test")
-    public @ResponseBody String returnHello() {
-        // This returns a JSON or XML with the users
-        return "Hello";
-    }
 }
