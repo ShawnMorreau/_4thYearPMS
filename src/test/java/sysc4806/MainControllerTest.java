02f536ee99af07644eef9a5669cec148223d7090
Craig Isesele <cmoisesele@live.com>
2018-03-20 21:50:38 -0400

    addes skeleton test code

---
+++ src/test/java/sysc4806/MainControllerTest.java
@@ -0,0 +1,16 @@
+package sysc4806;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by CraigBook on 2018-03-20.
+ */
+public class MainControllerTest {
+    @Test
+    public void returnHello() throws Exception {
+
+    }
+
+}
