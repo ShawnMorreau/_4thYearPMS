5c6b8a77fd0f636dba88d288381c7b6cb23fa07e
JaspreetSanghra <JaspreetSanghra@cmail.carleton.ca>
2018-03-21 22:38:04 -0400

    make Prof class entity

--- src/main/java/sysc4806/Prof.java
+++ src/main/java/sysc4806/Prof.java
@@ -1,23 +1,54 @@
 package sysc4806;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.*;
 /*
 *
  * Created by CraigBook on 2018-03-06.
 */
 
-
+@Entity
 public class Prof {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
     ProjectRepo_ project_repo;
     public Prof() {
-
+        this("");
     }
 
-    public Prof(ProjectRepo_ project_repo) {
+    public Prof(String name) {
+        this.name = name;
+    }
+
+    /*public Prof(ProjectRepo_ project_repo) {
         this.project_repo = project_repo;
         project_repo.addProf(this);
+    }*/
+
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+/*
     public void addProject(String title, String description, String programs, int studentLimit) {
         Project project_ = new Project(title, description, programs, studentLimit);
         project_repo.addProject(project_);
@@ -29,5 +60,5 @@ public class Prof {
 
     public void archiveProject(Project project_){
         project_repo.archiveProject(project_);
-    }
+    }*/
 }
