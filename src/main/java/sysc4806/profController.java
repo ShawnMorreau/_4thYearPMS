da7daf975ebb258904b7dc0c650bedcdac508936
damanveen <damanveen@gmail.com>
2018-03-21 21:37:17 -0400

    adding controller info

---
+++ src/main/java/sysc4806/profController.java
@@ -0,0 +1,24 @@
+package sysc4806;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping(path="/prof")
+public class profController {
+
+    @GetMapping(path="/test")
+    public @ResponseBody String returnHello() {
+        // This returns a JSON or XML with the users
+        return "Hello";
+    }
+
+
+
+}
