47466725cec0e1fa0ac3a5b119b1bafb94519631
JaspreetSanghra <JaspreetSanghra@cmail.carleton.ca>
2018-03-21 22:31:45 -0400

    fix prof repo

--- src/main/java/sysc4806/profRepo.java
+++ src/main/java/sysc4806/profRepo.java
@@ -2,6 +2,6 @@ package sysc4806;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface profRepo extends CrudRepository<Project, Long> {
+public interface profRepo extends CrudRepository<Prof, Long> {
 
 }
