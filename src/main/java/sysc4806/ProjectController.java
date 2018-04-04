5b43db60a818284717a90763eb0daff0da0fcbde
Craig Isesele <cmoisesele@live.com>
2018-04-04 15:59:13 -0400

    resetting ids across 3 contrller test classes

--- src/main/java/sysc4806/ProjectController.java
+++ src/main/java/sysc4806/ProjectController.java
@@ -1,5 +1,6 @@
 package sysc4806;
 
+import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
--- src/test/java/sysc4806/StudentControllerTest.java
+++ src/test/java/sysc4806/StudentControllerTest.java
@@ -1,5 +1,6 @@
 package sysc4806;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,9 +25,52 @@ public class StudentControllerTest {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    private static final String deleteAllProfsURL = "/prof/deleteAll";
+    private static final String deleteAllProjectsURL= "/project/deleteAll";
+    private static final String deleteAllStudentsURL= "/student/deleteAll";
+
+    private static final String url = "jdbc:mysql://localhost:3306/sysc";
+    Connection conn = null;
+    Statement st = null;
+
     @Before
     public void setUp() throws Exception {
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        String clearProfs = this.testRestTemplate.getForObject(deleteAllProfsURL, String.class);
+        System.out.println(clearProfs);
+        String clearProjects = this.testRestTemplate.getForObject(deleteAllProjectsURL, String.class);
+        System.out.println(clearProjects);
+//        String clearStudents = this.testRestTemplate.getForObject(deleteAllStudentsURL, String.class);
+//        System.out.println(clearStudents);
+
+
+        conn = DriverManager.getConnection(url, "root", "");
+
+        resetProfTable();
+        resetProjectTable();
+//        resetStudentTable();
+    }
+
+    public void resetProfTable() throws SQLException {
+
+        String resetProf = "ALTER TABLE PROF AUTO_INCREMENT = 1";
+        st = conn.createStatement();
+        st.executeUpdate(resetProf);
+    }
+
+    public void resetProjectTable() throws SQLException {
+        String resetProj = "ALTER TABLE PROJECT AUTO_INCREMENT = 1";
+        st = conn.createStatement();
+        st.executeUpdate(resetProj);
+    }
+
+    public void resetStudentTable() throws SQLException {
+        String resetStudent = "ALTER TABLE PROJECT AUTO_INCREMENT = 1";
+        st = conn.createStatement();
+        st.executeUpdate(resetStudent);
     }
 
     @Test
