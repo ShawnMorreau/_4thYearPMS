8b99d345c9a8fda3db180d72261b8b3786840eb4
ShawnMorreau <shawn.morreau@hotmail.com>
2018-04-04 21:54:21 -0400

    plain text passwords

--- src/main/java/sysc4806/UserServiceImpl.java
+++ src/main/java/sysc4806/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package sysc4806;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -23,8 +24,9 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public void saveUser(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+    public void saveUser(User user){
+
+        user.setPassword(user.getPassword());
         user.setActive(1);
         Role userRole = roleRepository.findByRole("STUDENT");
         user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
